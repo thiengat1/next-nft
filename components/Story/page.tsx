5809492e84dede09a22d1603d97f92be7bdbdc07
lewis <thiengat1@gmail.com>
2023-09-27 17:22:05 +0700

    fix build

--- components/Story/page.tsx
+++ components/Story/page.tsx
@@ -13,7 +13,7 @@ export default function Story(props: IStoryProps) {
           <h1 className=' text-5xl'>NFTs Story</h1>
           <p className='text-[24px]'>
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the industry s standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book.
           </p>
