2151730ffc422f29a0ee02c02f1b6c718df056ad
lewis <thiengat1@gmail.com>
2023-09-27 17:49:30 +0700

    fix build

--- components/Story/page.tsx
+++ components/Story/page.tsx
@@ -12,17 +12,12 @@ export default function Story(props: IStoryProps) {
         <div className='flex flex-col gap-6'>
           <h1 className=' text-5xl'>{`NFTs Story`}</h1>
           <p className='text-[24px]'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos magni
-            tenetur iusto tempora vero debitis cumque reiciendis ipsam. Eos
-            error iste nobis fugiat eveniet, fugit maxime officiis quisquam
-            consequuntur quo.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit Eos magni
+            tenetur iusto tempora vero debitis cumque reiciendis ipsam Eos error
+            iste nobis fugiat eveniet fugit maxime officiis quisquam
+            consequuntur quo
           </p>
-          <p className='text-[24px]'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias
-            temporibus velit repudiandae veniam minus tempora impedit sed eius
-            fugit aliquam maiores cupiditate in cumque nulla enim, ipsa
-            excepturi fugiat error.
-          </p>
+          <p className='text-[24px]'>Lorem ipsum dolor sit amet consectetur</p>
           <div>
             <button className='lewis-button w-[250px] bg-pinkColor'>
               {`Learn more`}
