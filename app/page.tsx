4cd7c04c8c13a046ff157b969856dc40f7bbd141
lewis <thiengat1@gmail.com>
2023-10-02 10:25:15 +0700

    fix for build

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Header from '@/components/Header/page';
 import Banner from '@/components/Banner/page';
-//import Story from '@/components/Story/page';
+import Story from '@/components/Story/page';
 import Collections from '@/components/Collections/page';
 import LiveAcution from '@/components/LiveAcution/page';
 import OurCreater from '@/components/OurCreater/page';
@@ -13,7 +13,7 @@ export default function Home() {
     <main className='flex min-h-screen flex-col  px-24 bg-main bg-cover text-white max-w-[1429px] mx-auto'>
       <Header />
       <Banner />
-      {/* <Story /> */}
+      <Story />
       <Collections />
       <LiveAcution />
       <OurCreater />
--- components/Story/page.tsx
+++ components/Story/page.tsx
@@ -10,16 +10,12 @@ export default function Story(props: IStoryProps) {
       <div className='flex mt-[150px]'>
         <img src='/assets/images/story.png' alt='story' />
         <div className='flex flex-col gap-6'>
-          <h1 className=' text-5xl'>{`NFTs Story`}</h1>
-          <p className='text-[24px]'>
-            {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`}
-          </p>
-          <p className='text-[24px]'>
-            {`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`}
-          </p>
+          <h1 className=' text-5xl'>Story</h1>
+          <p className='text-[24px]'>my story</p>
+          <p className='text-[24px]'>detail</p>
           <div>
             <button className='lewis-button w-[250px] bg-pinkColor'>
-              {`Learn more`}
+              detail
             </button>
           </div>
         </div>
