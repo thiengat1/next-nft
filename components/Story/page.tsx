180279c17b82934ed4d0f80c852543a62765537c
lewis <thiengat1@gmail.com>
2023-10-02 10:10:38 +0700

    init project

--- components/Story/page.tsx
+++ components/Story/page.tsx
@@ -12,12 +12,11 @@ export default function Story(props: IStoryProps) {
         <div className='flex flex-col gap-6'>
           <h1 className=' text-5xl'>{`NFTs Story`}</h1>
           <p className='text-[24px]'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit Eos magni
-            tenetur iusto tempora vero debitis cumque reiciendis ipsam Eos error
-            iste nobis fugiat eveniet fugit maxime officiis quisquam
-            consequuntur quo
+            {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`}
           </p>
-          <p className='text-[24px]'>Lorem ipsum dolor sit amet consectetur</p>
+          <p className='text-[24px]'>
+            {`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`}
+          </p>
           <div>
             <button className='lewis-button w-[250px] bg-pinkColor'>
               {`Learn more`}
